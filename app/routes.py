f1a81090c9e8a3197e2f6afe3b59d14bf702d133
Maciej Jastrzębski <macio.evo@gmail.com>
2023-06-07 16:28:09 +0200

    2023-06-07 MJ

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,24 @@
-from app import app 
+from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, Maciej Jastrzębski"
+    return render_template("index.html")
 
+@app.route('/extract')
+def extract():
+    return render_template("extract.html")
 
-@app.route('/name/<name>')
-def name(name):
-    return f"Hello, {name}!"
+@app.route('/product')
+def product():
+    return render_template("product.html")
+
+@app.route('/products')
+def products():
+    return render_template("products.html")
+
+@app.route('/author')
+def author():
+    return render_template("author.html")
+
